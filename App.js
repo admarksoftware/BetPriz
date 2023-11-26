c85be4a838768609eff6987de72488a6eeedcad8
cnrsbtogll <caner@qpien.com>
2023-11-26 20:00:06 +0300

    onesignal upgrade

--- App.js
+++ App.js
@@ -1,39 +1,24 @@
 import React, {useEffect} from 'react';
 import {SafeAreaView, StyleSheet, View} from 'react-native';
 import {WebView} from 'react-native-webview';
-import OneSignal from 'react-native-onesignal';
+import {LogLevel, OneSignal} from 'react-native-onesignal';
+
+// Remove this method to stop OneSignal Debugging
+OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+
+// OneSignal Initialization
+OneSignal.initialize('e7eda37f-ae50-41b1-a513-d88133ecfd19');
+
+// requestPermission will show the native iOS or Android notification permission prompt.
+// We recommend removing the following code and instead using an In-App Message to prompt for notification permission
+OneSignal.Notifications.requestPermission(true);
+
+// Method for listening for notification clicks
+OneSignal.Notifications.addEventListener('click', event => {
+  console.log('OneSignal: notification clicked:', event);
+});
 
 export default function App() {
-  useEffect(() => {
-    // OneSignal Initialization
-    OneSignal.setAppId('cb325789-3501-442a-9b1c-7a51852da407');
-
-    // promptForPushNotificationsWithUserResponse will show the native iOS or Android notification permission prompt.
-    // We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 8)
-    OneSignal.promptForPushNotificationsWithUserResponse();
-
-    //Method for handling notifications received while app in foreground
-    OneSignal.setNotificationWillShowInForegroundHandler(
-      notificationReceivedEvent => {
-        console.log(
-          'OneSignal: notification will show in foreground:',
-          notificationReceivedEvent,
-        );
-        let notification = notificationReceivedEvent.getNotification();
-        console.log('notification: ', notification);
-        const data = notification.additionalData;
-        console.log('additionalData: ', data);
-        // Complete with null means don't show a notification.
-        notificationReceivedEvent.complete(notification);
-      },
-    );
-
-    //Method for handling notifications opened
-    OneSignal.setNotificationOpenedHandler(notification => {
-      console.log('OneSignal: notification opened:', notification);
-    });
-  }, []);
-
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.wrapper}>
